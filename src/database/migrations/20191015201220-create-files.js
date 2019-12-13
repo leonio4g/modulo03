b3fd144a642fe040eedee5b39a71f1d5de5add9a
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2019-12-13 12:23:14 -0300

    chore: Adjust migrations

--- src/database/migrations/20191015201220-create-files.js
+++ src/database/migrations/20191015201220-create-files.js
@@ -1,18 +1,20 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('checkins', {
+    return queryInterface.createTable('files', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
       },
-      students_id: {
-        type: Sequelize.INTEGER,
-        references: { model: 'students', key: 'id' },
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
-        allowNull: true,
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      path: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
       },
       created_at: {
         type: Sequelize.DATE,
@@ -26,6 +28,6 @@ module.exports = {
   },
 
   down: queryInterface => {
-    return queryInterface.dropTable('checkins');
+    return queryInterface.dropTable('files');
   },
 };
